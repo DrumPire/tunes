f276b554bacba7de913412927132878168220277
Alex <expresproject@gmail.com>
2020-07-30 21:37:49 +0300

    fix total time in music player

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -29,6 +29,10 @@ export const musicPlayerInit = () => {
     } else {
       audioPlayer.play();
     }
+    
+    audioPlayer.addEventListener('canplay', () => {
+      updateTime();
+    });
   };
 
   const prevTrack = () => {
@@ -80,7 +84,7 @@ export const musicPlayerInit = () => {
     audioPlayer.play();
   });
 
-  audioPlayer.addEventListener('timeupdate', () => {
+  const updateTime = () => {
     const duration = audioPlayer.duration;
     const currentTime = audioPlayer.currentTime;
     const progress = (currentTime / duration) * 100;
@@ -95,7 +99,11 @@ export const musicPlayerInit = () => {
 
     audioTimePassed.textContent = `${addZero(minutesPassed)}:${addZero(secondsPassed)}`;
     audioTimeTotal.textContent = `${addZero(minutesTotal)}:${addZero(secondsTotal)}`;
-  });
+  };
+
+  updateTime();
+
+  audioPlayer.addEventListener('timeupdate', updateTime);
 
   audioProgress.addEventListener('click', event => {
     const x = event.offsetX;
